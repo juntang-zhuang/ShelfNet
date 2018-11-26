6299ef97d41d9fe51b83b42fc3eb4684b1109214
Juntang Zhuang <44451229+juntang-zhuang@users.noreply.github.com>
2018-11-25 23:07:14 -0500

    Update ade20k.py

--- encoding/datasets/ade20k.py
+++ encoding/datasets/ade20k.py
@@ -20,7 +20,7 @@ from .base import BaseDataset
 class ADE20KSegmentation(BaseDataset):
     BASE_DIR = 'ADEChallengeData2016'
     NUM_CLASS = 150
-    def __init__(self, root=os.path.expanduser('/data2/.encoding/data'), split='train',
+    def __init__(self, root=os.path.expanduser('~/.encoding/data'), split='train',
                  mode=None, transform=None, target_transform=None, **kwargs):
         super(ADE20KSegmentation, self).__init__(
             root, split, mode, transform, target_transform, **kwargs)
