1016a41286f6be415646a74e606e68eda4743835
Juntang Zhuang <44451229+juntang-zhuang@users.noreply.github.com>
2018-12-08 18:43:00 -0500

    Update __init__.py

--- encoding/datasets/__init__.py
+++ encoding/datasets/__init__.py
@@ -1,13 +1,19 @@
 from .base import *
+from .ade20k import ADE20KSegmentation
 from .pascal_voc import VOCSegmentation
 from .pascal_aug import VOCAugSegmentation
 from .pcontext import ContextSegmentation
 from .coco import COCOSegmentation
+from .cityscapes import CitySegmentation
+from .cityscapes_coarse import  CityCoarseSegmentation
 datasets = {
+    'ade20k': ADE20KSegmentation,
     'pascal_voc': VOCSegmentation,
     'pascal_aug': VOCAugSegmentation,
     'pcontext': ContextSegmentation,
     'coco':COCOSegmentation,
+    'citys': CitySegmentation,
+    'citys_coarse':CityCoarseSegmentation,
 }
 
 def get_segmentation_dataset(name, **kwargs):
